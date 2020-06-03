8f8f7ee652f7573ff0fe62e3a0f82a3f67b3859e
dannypas00 <dannypas00@outlook.com>
2020-06-03 11:09:09 +0200

    now moves, rotates 45 degrees and moves again

--- BungieSimulation/controllers/BungieController/MachineIntelligence/Strategies/GateStrategy.cpp
+++ BungieSimulation/controllers/BungieController/MachineIntelligence/Strategies/GateStrategy.cpp
@@ -4,21 +4,30 @@ namespace bungie {
 int GateStrategy::ExecuteStrategy()
 {
 	RobotController& controller = RobotController::getInstance();
-
-	if (rotated == 0) {
-		//Move 0.45m forwards to reach the rock
-		controller.MoveUnits(0.45, 'f', 1.5);
-		controller.Drive('r', 10);
-		rotated += 1;
-	}
-	else if (rotated >= 620) {
-		//Move 0.45m forwards to reach the container
-		controller.MoveUnits(0.45, 'f', 1.5);
-	}
-	else {
-		//Rotate 90 deg to the right
-		controller.Drive('r', 10);
-		rotated += 1;
+	if (wait > 0) {
+		if (distanceMoved < toMove) {
+			//Move 0.45m forwards to reach the rock
+			controller.Drive('f', 1.5);
+			if (distanceMoved++ >= toMove) wait = 1000;
+		}
+		else if (rotated == 0 && distanceMoved >= toMove && !hasRotatedFully) {
+			controller.Drive('r', 3);
+			rotated++;
+		}
+		else if (rotated >= toRotate) {
+			controller.Drive('f', 0);
+			hasRotatedFully = true;
+			distanceMoved = 0;
+			rotated = 0;
+		}
+		else if (!hasRotatedFully && rotated < toRotate) {
+			//Rotate 90 deg to the right
+			controller.Drive('r', 3);
+			if (rotated++ >= toRotate) wait = 1000;
+		}
+	} else {
+		controller.Drive('f', 0);
+		wait++;
 	}
 	return 0;
 }
--- BungieSimulation/controllers/BungieController/MachineIntelligence/Strategies/GateStrategy.h
+++ BungieSimulation/controllers/BungieController/MachineIntelligence/Strategies/GateStrategy.h
@@ -10,7 +10,12 @@ public:
 	int ExecuteStrategy() override;
 
 private:
+	const double toRotate = 467;
+	const double toMove = 450;
+	bool hasRotatedFully;
 	double rotated = 0;
+	double distanceMoved = 0;
+	double wait = 0;
 };
 }
 
