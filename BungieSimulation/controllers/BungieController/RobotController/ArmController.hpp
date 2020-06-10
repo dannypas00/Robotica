39a1f357d0801985686b63e812f15457b6e39cae
shcalbers <34623185+shcalbers@users.noreply.github.com>
2020-06-10 12:53:04 +0200

    Document and Refactor ArmController

--- BungieSimulation/controllers/BungieController/RobotController/ArmController.hpp
+++ BungieSimulation/controllers/BungieController/RobotController/ArmController.hpp
@@ -6,25 +6,38 @@
 
 namespace bungie {
 
+/// @brief Class used for controlling the arm of the Bungie Rover.
 class ArmController {
-  public:
-  ArmController(webots::Robot *robot);
-  ~ArmController();
+
+public:
+
+  /// @brief Constructs a new ArmController instance.
+  /// @param webots::Robot* A pointer to the robot containing the arm to control.
+  ArmController(webots::Robot* robot);
   
-  void MoveArm(double velocity);
+  /// @brief Moves the arm into the given direction.
+  /// @param direction The direction to turn the arm into.
+  /// @param velocity The velocity, in radians per second, at which to rotate the arm.
   void PowerJointMotors(char direction, double velocity);
   
-  private:
+  /// @brief Deconstructs the ArmController instance.
+  ~ArmController();
+
+private:
+
+  /// @brief An enum, enumerating every motor used in the arm.
   enum Motor {
       ARM_ROTATION_MOTOR, ARM_BASE_MOTOR, 
       ARM_MID_MOTOR, ARM_END_MOTOR,
       TOTAL_MOTORS
-    };
-    
-  double currentPos;
-    
+  };
+
+  /// @brief The motors powering the arm.
   webots::Motor* motors[TOTAL_MOTORS];
+
 };
+
 }
 
 #endif
+
